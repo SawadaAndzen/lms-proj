f5f67053406cb96238a31b9db6a10105b3f65ba1
Sawada <105200073+SawadaAndzen@users.noreply.github.com>
2025-01-16 12:56:33 +0200

    added corp. style + profile + model + media

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import static
+from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path, include
 
@@ -28,4 +28,4 @@ urlpatterns = [
     #path('modules/', include('modules.urls')),
     #path('lessons/', include('lessons.urls')),
     #path('tasks/', include('tasks.urls')),
-] # + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- root/views.py
+++ root/views.py
@@ -1,10 +1,12 @@
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth.models import User
-from django.contrib.auth.views import LoginView
-from django.views.generic import CreateView
-from .forms import CustomSignUpForm, CustomLoginForm
+from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.views.generic import CreateView, DetailView, UpdateView
+from .forms import CustomSignUpForm, CustomLoginForm, ProfileForm, CustomChangePasswordForm, CustomUserChangeForm
+from .models import Profile
 
 
+#AUTH
 class SignUpView(CreateView):
     model = User
     form_class = CustomSignUpForm
@@ -26,3 +28,72 @@ class CustomLoginView(LoginView):
         self.request.session['success_message'] = f"Hi, {self.request.user.username}!"
         
         return reverse('index')
+    
+    
+#PROFILE
+class ProfileDetailView(DetailView):
+    model = Profile
+    template_name = 'root/profile/profile.html'
+    context_object_name = 'profile'
+
+    def get_object(self):
+        username = self.kwargs.get('username', self.request.user.username)
+        user = User.objects.get(username=username)
+        profile, created = Profile.objects.get_or_create(user=user)
+        
+        self.request.session['last_viewed'] = profile.id
+        
+        return profile
+
+
+class UpdateProfileView(UpdateView):
+    model = Profile
+    form_class = ProfileForm
+    template_name = 'root/profile/update_profile.html'
+    
+    def get_success_url(self):
+        return reverse_lazy('profile', kwargs={'username': self.object.user.username})
+
+    def get_object(self, queryset = None):
+        user = self.request.user
+        profile, created = Profile.objects.get_or_create(user=user)
+        
+        return profile
+
+    def form_valid(self, form):
+        profile = form.save(commit = False)
+        profile.user = self.request.user
+        profile.save()
+        
+        if not profile.pfp:
+            profile.pfp = 'static/profile/default-pfp.jpg'
+        profile.save()
+        
+        self.request.session['success_message'] = 'Success'
+        
+        del self.request.session['success_message']
+        
+        return super().form_valid(form)
+    
+    
+#USER CUSTOMIZATION
+class UserUpdate(UpdateView):
+    model = User
+    form_class = CustomUserChangeForm
+    template_name = 'root/user/change_user.html'
+    
+    def get_success_url(self):
+        return reverse_lazy('profile', kwargs={'username': self.object.username})
+    
+    def get_object(self, queryset=None):
+        username = self.kwargs.get('username')
+        
+        return User.objects.get(username = username)
+    
+    
+class CustomPasswordView(PasswordChangeView):
+    template_name = 'root/user/change_password.html'
+    form_class = CustomChangePasswordForm
+    
+    def get_success_url(self):
+        return reverse_lazy('profile', kwargs={'username': self.request.user.username})
