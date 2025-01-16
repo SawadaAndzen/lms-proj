ef2765f58887c3afae544618aff1c2b81dca58cf
Sawada <105200073+SawadaAndzen@users.noreply.github.com>
2025-01-16 14:46:07 +0200

    module model

--- modules/models.py
+++ modules/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from lessons.models import Lesson
 
-# Create your models here.
+
+class Module(models.Model):
+    name = models.CharField(max_length = 50)
+    lessons = models.ManyToManyField(Lesson, blank = True, null = True)
+    
+    def __str__(self):
+        return self.name
