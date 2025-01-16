7c835108c6983e84b80680b1091147f64755e3bc
Sawada <105200073+SawadaAndzen@users.noreply.github.com>
2025-01-16 13:33:27 +0200

    class/group model (wip)

--- classes/models.py
+++ classes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from root.models import Role
 
-# Create your models here.
+
+class Class(models.Model):
+    name = models.CharField(max_length = 50)
+    desc = models.TextField()
+    course = ...
+    teacher = models.ForeignKey(
+        User, 
+        on_delete = models.SET_NULL, 
+        null = True, 
+        limit_choices_to = {'role__role': 'teacher'},
+        related_name = "teacher"
+    )
+    students = models.ManyToManyField(
+        User, 
+        blank = True, 
+        limit_choices_to = {'role__role': 'student'},
+        related_name = "students"
+    )
+    
+    def __str__(self):
+        return self.name
