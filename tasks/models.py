010877f77bd5da1c7a67e0ccc813ca937f8ca01b
Sawada <105200073+SawadaAndzen@users.noreply.github.com>
2025-01-16 14:35:58 +0200

    task model (wip)

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    lesson = ...
+    name = models.CharField(max_length = 50)
+    desc = models.TextField()
+    media = models.FileField(upload_to = 'tasks/files/', null = True, blank = True)
+    is_done = models.BooleanField(default = False)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class TaskAnswer(models.Model):
+    task = models.ForeignKey(Task, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    media = models.FileField(upload_to = 'tasks/answers/', blank = True, null = True)
+    
+    def __str__(self):
+        return f"@{self.user.username}'s answer for {self.task}"
