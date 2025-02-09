b3ce04f80313c28f70c0b2a18d9ebd154ad8a9ca
Sawada <105200073+SawadaAndzen@users.noreply.github.com>
2025-02-09 14:32:33 +0200

    root tests

--- root/tests.py
+++ root/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Create your tests here.
+
+class RootTest(TestCase):
+    def test_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        
+        
+class ModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="user", password="password")
+        self.profile = Profile.objects.create(user=self.user, pfp=None, desc=".", cash=100.00)
+    
+    def test_profile(self):
+        self.assertEqual(self.profile.user.username, "user")
+        self.assertEqual(self.profile.desc, ".")
+        self.assertEqual(self.profile.cash, 100.00)
+        
+    def test_profile_str(self):
+        self.assertEqual(str(self.profile), "@user's profile")
